df45f86a7982be1820ae61ab5be8e03611eb1e59
Tarun447 <tarunkumarnanda5@gmail.com>
2023-10-29 23:12:09 +0530

    First Commit

---
+++ Java_App_Jenkins/src/com/jenkins/main/MainClass.java
@@ -0,0 +1,13 @@
+package com.jenkins.main;
+
+import java.util.Date;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Java First Projetc");
+		System.out.println("Example For Jenkins App");
+		System.out.println(new Date());
+	}
+}
