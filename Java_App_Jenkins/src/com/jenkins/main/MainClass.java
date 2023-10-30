cdbd769be2af705a88c58e189b38584a447a300d
Tarun447 <tarunkumarnanda5@gmail.com>
2023-10-30 21:56:59 +0530

    New Code Commits

--- Java_App_Jenkins/src/com/jenkins/main/MainClass.java
+++ Java_App_Jenkins/src/com/jenkins/main/MainClass.java
@@ -8,6 +8,7 @@ public class MainClass {
 		
 		System.out.println("Welcome to Java First Projetc");
 		System.out.println("Example For Jenkins App");
+		System.out.println("New Line of Code added");
 		System.out.println(new Date());
 	}
 }
